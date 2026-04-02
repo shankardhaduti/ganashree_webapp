f16f13a3dfae01f6c8063d9a46561bf531535026
alwayspratham <prathamkoparde@gmail.com>
2026-04-02 11:34:52 +0530

    Remove ImpactPage component from About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ImpactPage from "./About_components/ImpactPage";
+
 import OurJourney from "./About_components/OurJourney";
 import CommitmentCards from "./About_components/CommitmentCards";
 
 function About() {
   return (
     <section id="about">
-      <ImpactPage />
+      
       <OurJourney />
       <CommitmentCards />
     </section>
